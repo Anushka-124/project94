4a3e681e73374de605b0eea6fb2e2d7499965272
Anushka-124 <83501692+Anushka-124@users.noreply.github.com>
2022-12-22 17:55:13 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1oFhhby5_X9fnTFShYb1-aZz10CiLnlU",
+    authDomain: "letschat-c3dd1.firebaseapp.com",
+    projectId: "letschat-c3dd1",
+    storageBucket: "letschat-c3dd1.appspot.com",
+    messagingSenderId: "228056316760",
+    appId: "1:228056316760:web:b761b1118e27cacdcd032d",
+    measurementId: "G-YJ65QWT2MZ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
